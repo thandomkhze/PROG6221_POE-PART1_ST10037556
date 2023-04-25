752c59ad2944f9e12c774709fea52fd01ab874f3
thandomkhze <102976043+thandomkhze@users.noreply.github.com>
2023-04-25 17:29:40 +0200

    ingredients are now collected in "Ingredients()". Method "collectIngredients" has changed to "setIngredients". A get method has been added. methods hoa been added to conferm the name,quantity and unit of messurment. Methods ("isEmpty","checkFloat") have been created to check if the values are valid.

--- POE/Ingredients.cs
+++ POE/Ingredients.cs
@@ -5,46 +5,126 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace POE
 {
     class Ingredients
     {
-        ArrayList[] totalIngredient;
-        ArrayList singleIngredient;
+        private List<ArrayList> totalIngredient = new List<ArrayList>();
+        private ArrayList singleIngredient;
+
         public Ingredients()
-        {
-            collectIngredients();
-        }
-        public void collectIngredients()
         {
             Console.WriteLine("Enter the number of ingredients.");
             int numIngredients = Convert.ToInt32(Console.ReadLine());
-            totalIngredient = new ArrayList[numIngredients];
-            String name = "", unitOfMeasurement = "";
-            double quantity = 0;
-
+            setIngredients(numIngredients);
+        }
+        public void setIngredients(int numIngredients)
+        {
             for (int k = 0; k < numIngredients; k++)
             {
-                Boolean confermName = false, confermQuantity = false, confermUnit = false;
-
+                String name = "", unitOfMeasurement = "", quantity = "";
                 singleIngredient = new ArrayList();
                 Console.WriteLine("Ingredient No." + (k + 1));
-                
-                Console.Write("Ingredient name : ");
-                name = Console.ReadLine();
-               
-                Console.Write("Unit of measurement : ");
-                    unitOfMeasurement = Console.ReadLine();
-                
-                Console.Write("Amount of " + unitOfMeasurement + " : ");
-                quantity = Convert.ToDouble(Console.ReadLine());
-                
+
+                confermName(name);
+                confermUnit(unitOfMeasurement);
+                confermQuantity(quantity, unitOfMeasurement);
+
                 singleIngredient.Add(name);
                 singleIngredient.Add(unitOfMeasurement);
                 singleIngredient.Add(quantity);
-                totalIngredient[k] = singleIngredient;
+                totalIngredient.Add(singleIngredient);
             }
+
+        }
+
+        public List<ArrayList> getIngredients()
+        {
+            return totalIngredient;
+        }
+
+        private String confermName(String name)
+        {
+            Boolean conferm = false;
+            while (!conferm)
+            {
+                Console.Write("Ingredient name : ");
+                name = Console.ReadLine().Trim();
+
+                if (isEmpty(name))
+                {
+                    conferm = false;
+                    Console.WriteLine("The name has not been entered");
+                }
+                else
+                    conferm = true;
+            }
+            return name;
+        }
+
+        private double confermQuantity(String quantity, String unit)
+        {
+            Boolean conferm = false;
+            double q = 0.0;
+            while (!conferm)
+            {
+                Console.Write("Amount of " + unit + " : ");
+                quantity = Console.ReadLine();
+
+                if (checkFloat(quantity))
+                {
+                    conferm = true;
+                    q = Convert.ToDouble(quantity);
+                }
+            }
+            return q;
+        }
+
+        private String confermUnit(String unit)
+        {
+            Boolean conferm = false;
+            while (!conferm)
+            {
+                Console.Write("Unit of measurement : ");
+                unit = Console.ReadLine();
+
+                if (isEmpty(unit))
+                {
+                    conferm = false;
+                    Console.WriteLine("The unit of messurement has not been entered");
+                }
+                else
+                    conferm = true;
+            }
+            return unit;
+        }
+
+        private Boolean isEmpty(String s)
+        {
+            if (s == null && s == "")
+                return true;
+            else
+                return false;
+        }
+
+        private Boolean checkFloat(String d)
+        {
+            try
+            {
+                double value = Convert.ToDouble(d);
+                if (value > 0)
+                    return true;
+                else
+                    Console.WriteLine("The value needs to ba a positive number");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Input needs to be a number");
+            }
+
+            return false;
         }
 
     }
