5ba0c88cd3e4238040dc2887a854692dd8cd920a
thandomkhze <102976043+thandomkhze@users.noreply.github.com>
2023-04-26 14:58:46 +0200

    Class "Steps" has been created and has inherited from abstract class "Check". number of steps is collected from the user.  Get ans set methods have been created within the "Steps" class

---
+++ POE/Steps.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE
+{
+    public class Steps : Checks
+    {
+        public Steps()
+        {
+            Boolean stepNoEntered = false;
+            while (!stepNoEntered)
+            {
+                Console.WriteLine("Enter the number of steps.");
+                String placeHolder = Console.ReadLine();
+                int numSteps = 0;
+                if (!isEmpty(placeHolder))
+                    if (checkFloat(placeHolder, true))
+                        numSteps = Convert.ToInt32(placeHolder);
+                setSteps(numSteps);
+            }
+        }
+
+        private void setSteps(int numSteps)
+        {
+
+        }
+        public String getSteps()
+        {
+            return "";
+        }
+    }
+}
