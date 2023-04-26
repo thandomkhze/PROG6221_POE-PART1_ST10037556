736940b86e34f5886114264ca7690016501edea7
thandomkhze <102976043+thandomkhze@users.noreply.github.com>
2023-04-26 17:10:13 +0200

    array "totalSteps" was added onto the "Steps" class. the whileloop was corrected to prevent an infinite loop within the "Steps" class. Steps are collected in the "setSteps" method with the help of the "isEmpty" and "checkFloat" methods from the "Check" class.

--- POE/Steps.cs
+++ POE/Steps.cs
@@ -1,35 +1,65 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace POE
 {
     public class Steps : Checks
     {
+        private String[] totalSteps;
         public Steps()
         {
+            int numSteps = 0;
             Boolean stepNoEntered = false;
             while (!stepNoEntered)
             {
                 Console.WriteLine("Enter the number of steps.");
                 String placeHolder = Console.ReadLine();
-                int numSteps = 0;
+
                 if (!isEmpty(placeHolder))
                     if (checkFloat(placeHolder, true))
+                    {
                         numSteps = Convert.ToInt32(placeHolder);
-                setSteps(numSteps);
+                        stepNoEntered = true;
+                    }
             }
+            totalSteps = new String[numSteps];
+            setSteps(numSteps);
         }
 
         private void setSteps(int numSteps)
         {
+            for (int k = 0; k < numSteps; k++)
+            {
+                String description = "";
+                Console.WriteLine("Step No." + (k + 1));
 
+                Boolean conferm = false;
+                while (!conferm)
+                {
+                    Console.WriteLine("Step no.{0} description :", k + 1);
+                    description = Console.ReadLine();
+
+                    if (isEmpty(description))
+                    {
+                        conferm = false;
+                        Console.WriteLine("A description has not been entered");
+                    }
+                    else
+                        conferm = true;
+                }
+
+                totalSteps[k] = (description);
+            }
         }
-        public String getSteps()
+
+        public String[] getSteps()
         {
-            return "";
+            return totalSteps;
         }
     }
 }
