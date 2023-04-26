3aa13d9a22320994854506b0da0a155225fdb15b
thandomkhze <102976043+thandomkhze@users.noreply.github.com>
2023-04-26 17:56:59 +0200

    Class "Steps" is declared in the main class and the list of steps and ingredients are stored in two arrays. A method named "display" was created in the main method to display the ingredients and steps.

--- POE/Program.cs
+++ POE/Program.cs
@@ -1,9 +1,30 @@
 ﻿using POE;
+using System.Collections;
 
 public class Program
 {
     public static void Main(String[] args)
     {
         Ingredients ingredients = new Ingredients();
+        ArrayList[] totalIngridients = ingredients.getIngredients();
+        Steps steps = new Steps();
+        String[] totalStep = steps.getSteps();
+        display(totalIngridients, totalStep);
+    }
+
+    private static void display(ArrayList[] ingredients, String[] Steps)
+    {
+        Console.WriteLine("=========================================================================================");
+        Console.WriteLine("Ingredients:");
+        for (int k = 0; k < ingredients.Count(); k++)
+        {
+            Console.WriteLine("\t * {0} {1}/s of {2} ", ingredients[k][1], ingredients[k][2], ingredients[k][0]);
+        }
+        Console.WriteLine("\nSteps:");
+        for (int j = 0; j < Steps.Count(); j++)
+        {
+            Console.WriteLine("\tStep {0}:\n\t{1}", j + 1, Steps[j]);
+        }
+        Console.WriteLine("=========================================================================================");
     }
 }
