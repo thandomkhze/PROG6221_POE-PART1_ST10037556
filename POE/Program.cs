2510f29cd8e64ac0f65f3fab235250fb7b36ff9c
thandomkhze <102976043+thandomkhze@users.noreply.github.com>
2023-04-28 19:00:35 +0200

    final commit

--- POE/Program.cs
+++ POE/Program.cs
@@ -3,16 +3,66 @@ using System.Collections;
 
 public class Program
 {
+    private Ingredients ingredients;
+    private Steps steps;
+
+    private ArrayList[] totalIngridients;
+
+
+    private String[] totalStep;
     public static void Main(String[] args)
     {
-        Ingredients ingredients = new Ingredients();
-        ArrayList[] totalIngridients = ingredients.getIngredients();
-        Steps steps = new Steps();
-        String[] totalStep = steps.getSteps();
+        new Program().runApp();
+    }
+
+    private void runApp()
+    {
+        collectAndDisplay();
+        Boolean done = false;
+        while (!done)
+        {
+            int choice = 0;
+
+            Console.WriteLine("What do you wish to do?\n" +
+                "1) Enter a new recipe.\n" +
+                "2) Change the scale of the recipe.\n" +
+                "3) Reset quantities to the original values.\n" +
+                "4)Exit");
+
+            choice = Convert.ToInt32(Console.ReadLine());
+            // case statement which will choose a method that matches the selected option.
+            switch (choice)
+            {
+                case 1:
+                    collectAndDisplay();
+                    break;
+                case 2:
+                    display(scale(totalIngridients), totalStep);
+                    break;
+                case 3:
+                    display(totalIngridients, totalStep);
+                    break;
+                case 4:
+                    done = true;
+                    break;
+            }
+        }
+    }
+    //method for collecting and displaying the ingredients and steps with the aid of a method called display().
+    private void collectAndDisplay()
+    {
+        Console.Clear();
+        ingredients = new Ingredients();
+        totalIngridients = ingredients.getIngredients();
+        //originalQuantity = ingredients.getIngredients();
+
+        steps = new Steps();
+        totalStep = steps.getSteps();
+
         display(totalIngridients, totalStep);
     }
 
-    private static void display(ArrayList[] ingredients, String[] Steps)
+    private void display(ArrayList[] ingredients, String[] Steps)
     {
         Console.WriteLine("=========================================================================================");
         Console.WriteLine("Ingredients:");
@@ -23,13 +73,15 @@ public class Program
         Console.WriteLine("\nSteps:");
         for (int j = 0; j < Steps.Count(); j++)
         {
-            Console.WriteLine("\tStep {0}:\n\t{1}", j + 1, Steps[j]);
+            Console.WriteLine("\tStep {0}:\n\t{1}\n", j + 1, Steps[j]);
         }
-        Console.WriteLine("=========================================================================================");
+        Console.WriteLine("=========================================================================================\n");
+
     }
+    //method for scaling the quantities(with the aid of the scaleConversion method.)
     private ArrayList[] scale(ArrayList[] arrIngredient)
     {
-        Console.WriteLine("Select scale :\n1) 0.5\n2) 2\n3) 3");
+        Console.WriteLine("Select scale :\n\t1) 0.5\n\t2) 2\n\t3) 3");
         int option = Convert.ToInt32(Console.ReadLine());
 
         switch (option)
@@ -40,17 +92,16 @@ public class Program
         }
         return arrIngredient;
     }
-    private ArrayList[] scaleConversion(double scale, ArrayList[] arrIngredient)
+    private static ArrayList[] scaleConversion(double scale, ArrayList[] arrIngredient)
     {
-        for (int k = 0; k < arrIngredient.Count(); k++)
-            arrIngredient[k][1] = (double)arrIngredient[k][1] * scale;
-        return arrIngredient;
-    }
+        ArrayList[] ChangedQuantity = new ArrayList[arrIngredient.Count()];
 
-    private ArrayList[] resetQuantity(ArrayList[] arrIngredient, double[] quantity)
-    {
-        for (int k = 0; k < quantity.Count(); k++)
-            arrIngredient[k][1] = quantity[k];
-        return arrIngredient;
+        ChangedQuantity = arrIngredient;
+
+        for (int k = 0; k < ChangedQuantity.Count(); k++)
+            ChangedQuantity[k][1] = (double)ChangedQuantity[k][1] * scale;
+        Console.WriteLine("Scaled by " + scale);
+
+        return ChangedQuantity;
     }
 }
